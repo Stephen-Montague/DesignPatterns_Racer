5f91f336683d886fa40fd6ac2353a1ffc49d327f
Stephen-Montague <54737111+Stephen-Montague@users.noreply.github.com>
2019-10-19 17:58:50 +0900

    Update module-info.java

--- lewisu.edu-racing_sim/src/module-info.java
+++ lewisu.edu-racing_sim/src/module-info.java
@@ -1,7 +1,6 @@
 module racingSim {
     requires transitive javafx.controls;
-	requires org.junit.jupiter.api;
-	requires junit;
+    requires org.junit.jupiter.api;
     exports racingSim.terrain;
     exports racingSim;
 }
